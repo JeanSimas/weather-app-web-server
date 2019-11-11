e4940483d21158f77db927817a8fc4d5dede39bb
jeansimas <jeansimas12@hotmail.com>
2019-11-11 14:24:39 -0300

    showing the result on the page

--- public/js/app.js
+++ public/js/app.js
@@ -1,17 +1,26 @@
+const weatherForm = document.querySelector('form')
+const searchElement = document.querySelector('input')
+
+const messageOne = document.querySelector('#message-one')
+const messageTwo = document.querySelector('#message-two')
+
+
 showInformation = (location) => {
+    messageOne.textContent = 'Loading Forecast'
     fetch('http://localhost:3000/weather?address=' + location).then((response) => {
         response.json().then((parsedData) => {
             if (parsedData.error) {
-                console.log(parsedData.error)
+                messageOne.textContent = 'Error'
+                messageTwo.textContent = parsedData.error
             } else {
-                console.log(parsedData.forecast, parsedData.location)
+                messageOne.textContent = parsedData.location
+                messageTwo.textContent = parsedData.forecast
+
             }
         })
     })
 
 }
-const weatherForm = document.querySelector('form')
-const searchElement = document.querySelector('input')
 
 weatherForm.addEventListener('submit', (event) => {
     event.preventDefault()
